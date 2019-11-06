3eabb6c7136d2d361a4321a092bc4a7bd49480cc
devmycf <informatica@carflet.es>
2019-11-06 18:54:52 +0100

    api

---
+++ server/cfsettings_example.js
@@ -0,0 +1,23 @@
+export const mytesting = {
+    "emails": [
+        {
+            "key": "",
+            "value": "",
+        },
+        {
+            "key": "",
+            "value": "",
+        },
+        {
+            "key": "",
+            "value": "",
+        },
+    ],
+
+    "sqlconfig": {
+        "host": "",
+        "usr": "",
+        "pwd": "",
+        "db": ""
+    }
+}
